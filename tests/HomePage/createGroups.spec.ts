d8a0206d50dd7905bc0ee58710c8c90c94fc251f
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2024-02-07 14:21:45 +0000

    principal tests

---
+++ tests/HomePage/createGroups.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+
+
+test.describe('Create Group', () => {
+
+  test('should create a new group', async ({page}) => {
+    await page.goto('/');
+    await page.getByRole('heading', { name: 'Gastos Compartidos' }).isVisible();
+    await page.getByRole('button', { name: 'Nuevo Grupo' }).click();
+    await page.getByPlaceholder('Título').fill('Test Grupo');
+    await page.getByPlaceholder('Descripción').fill('Test descripci');
+    await page.getByPlaceholder('Nombre del participante').click();
+    await page.getByPlaceholder('Nombre del participante').fill('Oscar');
+    await page.getByRole('button', { name: 'Añadir Participante' }).click();
+    await page.getByPlaceholder('Nombre del participante 2').fill('Richard');
+    await page.getByRole('button', { name: 'Añadir Participante' }).click();
+    await page.getByPlaceholder('Nombre del participante 3').fill('Adrián');
+    await page.getByRole('button', { name: 'crear' }).click();
+    await page.getByRole('heading', { name: 'Test Grupo' }).isVisible();
+    await page.getByRole('heading', { name: 'Test descripción grupo' }).isVisible();
+  });
+
+  // Create a test with playwright similar to the one above, but this time, when create a participant, delete it.
+  test('should create a new group and delete a participant in the form', async ({page}) => {
+    await page.goto('/');
+    await page.getByRole('button', { name: 'Nuevo Grupo' }).click();
+    await page.getByPlaceholder('Título').fill('prueba2');
+    await page.getByPlaceholder('Descripción').fill('descripción 2');
+    await page.getByPlaceholder('Nombre del participante').click();
+    await page.getByPlaceholder('Nombre del participante').fill('Oscar');
+    await page.getByRole('button', { name: 'Añadir Participante' }).click();
+    await page.getByPlaceholder('Nombre del participante 2').click();
+    await page.getByPlaceholder('Nombre del participante 2').fill('Juan');
+    const participantsForm = await page.getByText('ParticipantesAñadir');
+    await participantsForm.getByRole('button').first().click();
+    await page.getByRole('button', { name: 'Crear' }).click();
+    await page.getByRole('heading', { name: 'Prueba2' }).isVisible();
+    await page.getByRole('heading', { name: 'descripción 2' }).isVisible();
+  });
+})
