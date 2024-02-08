5769a486ef00a992e3e057069b7dd373bf25772c
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2024-02-08 03:18:42 +0000

    change name test

--- src/modules/Group/application/GroupService.spec.ts
+++ src/modules/Group/application/GroupService.spec.ts
@@ -1,10 +1,10 @@
-import { describe, it, expect } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { GroupService } from './GroupService'
 import { expectedExpensesDebts, expenseRepository, expenses, groupRepository, users } from './mockData'
 
-describe.only('GroupService', () => {
+describe.only('Group expenses example challenge', () => {
   const groupService = new GroupService(groupRepository, expenseRepository)
-  it('should calculate the total expenses by participants', () => {
+  test('should calculate the total expenses by participants', () => {
     const totalExpenses = groupService.totalExpensesByParticipant(users, expenses)
 
     const expected: typeof totalExpenses = [
@@ -17,13 +17,13 @@ describe.only('GroupService', () => {
     expect(totalExpenses).toEqual(expected)
   })
 
-  it('should calculate the average of expenses between all participants', () => {
+  test('should calculate the average of expenses between all participants', () => {
     const average = groupService.calculateAverageExpenses(expenses, users)
     const expected: number = 40.85
     expect(average).toEqual(expected)
   })
 
-  it('should calculate the debts of each participant', () => {
+  test('should calculate the debts of each participant', () => {
     const totalExpensesByParticipant = [
       { id: '1', name: 'Fran', totalExpense: 100 },
       { id: '2', name: 'Alfonso', totalExpense: 63.4 },
@@ -36,7 +36,7 @@ describe.only('GroupService', () => {
     expect(expectedExpensesDebts).toEqual(debts)
   })
 
-  it('should calculate all expenses debts', () => {
+  test('should calculate all expenses debts', () => {
     const finalExpensesParticipants = groupService.calculateExpenses(users, expenses)
     expect(expectedExpensesDebts).toEqual(finalExpensesParticipants)
   })
