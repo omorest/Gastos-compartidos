7879d692811fac3e91f95d2f20915f2ee97f2c83
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2024-03-15 15:57:17 +0000

    refactor: usecase getgroupby id instead group service

--- src/modules/Group/ui/hooks/useGroup.ts
+++ src/modules/Group/ui/hooks/useGroup.ts
@@ -1,14 +1,15 @@
 import { type UseQueryResult, useQuery } from '@tanstack/react-query'
 import { type Group } from '../../domain/Group'
-import { useGroupService } from '../../../../hooks/GroupServiceContext/useGroupService'
+import { useCasesGroup } from './useCasesGroup'
 
 export const useGroup = (groupId: string | undefined): UseQueryResult<Group | null, Error> => {
-  const groupService = useGroupService()
+  const { getGroupByIdQuery } = useCasesGroup()
+
   const query = useQuery({
     queryKey: ['group'],
     queryFn: async () => {
       if (groupId) {
-        return await groupService.get(groupId)
+        return await getGroupByIdQuery.execute(groupId)
       } else {
         throw new Error('No group id')
       }
