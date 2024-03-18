960ee5268a1d9b122c25c441256659f305cda6ef
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2024-03-18 09:31:54 +0000

    refactor: removegroup with use case

--- src/modules/Group/ui/hooks/useRemoveGroup.ts
+++ src/modules/Group/ui/hooks/useRemoveGroup.ts
@@ -1,13 +1,13 @@
 import { useMutation, useQueryClient, type UseMutationResult } from '@tanstack/react-query'
 import { type Group } from '../../domain/Group'
-import { useGroupService } from '../../../../hooks/GroupServiceContext/useGroupService'
+import { useCasesGroup } from './useCasesGroup'
 
 export const useRemoveGroup = (): { removeGroupMutation: UseMutationResult<void, Error, Group, unknown> } => {
-  const groupService = useGroupService()
+  const { removeGroupCommand } = useCasesGroup()
   const queryClient = useQueryClient()
 
   const removeGroupMutation = useMutation({
-    mutationFn: async (group: Group) => { await groupService.remove(group.id) },
+    mutationFn: async (group: Group) => { await removeGroupCommand.execute(group.id) },
     onSuccess: async () => { await queryClient.invalidateQueries({ queryKey: ['groups'] }) }
   })
 
