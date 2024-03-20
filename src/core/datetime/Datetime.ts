ca475c55c508e4fa19fb3cdeb84d4c6afbf24a4c
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2024-03-20 15:18:11 +0000

    feat: new method in datetime to formatDate for inputs

--- src/core/datetime/Datetime.ts
+++ src/core/datetime/Datetime.ts
@@ -2,4 +2,8 @@ export class Datetime {
   static format (date: Date): string {
     return date.toLocaleDateString('es-ES', { day: 'numeric', month: 'numeric', year: 'numeric' })
   }
+
+  static formatForInput (date: Date): string {
+    return date.toISOString().split('T')[0]
+  }
 }
