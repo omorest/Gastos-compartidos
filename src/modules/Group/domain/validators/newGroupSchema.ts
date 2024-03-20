bd7aa866294f1f6018dce267d8d9bf037707eba8
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2024-03-20 11:56:34 +0000

    feat: zod schema form new group

---
+++ src/modules/Group/domain/validators/newGroupSchema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+
+export const NewGroupSchema = z.object({
+  name: z.string().min(1, 'Este campo es requerido').max(50, 'Máximo 50 caracteres'),
+  description: z.string().max(100, 'Máximo 50 caracteres').optional(),
+  participants: z.array(
+    z.object({
+      name: z.string().min(1, 'Este campo es requerido').max(30, 'Máximo 30 caracteres')
+    })
+  )
+})
+
+export type NewGroupSchemaType = z.infer<typeof NewGroupSchema>
