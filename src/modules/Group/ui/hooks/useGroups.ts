1ccd211ba21da096a80c2afac9ef047056695401
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2024-03-21 16:09:04 +0000

    feat: typing queries keys

--- src/modules/Group/ui/hooks/useGroups.ts
+++ src/modules/Group/ui/hooks/useGroups.ts
@@ -1,11 +1,12 @@
 import { type UseQueryResult, useQuery } from '@tanstack/react-query'
 import { type Group } from '../../domain/Group'
 import { useCasesGroup } from './useCasesGroup'
+import { type ValidQueryKeys } from '../../../../types.queries'
 
 export const useGroups = (): UseQueryResult<Group[], Error> => {
   const { getAllGroupsQuery } = useCasesGroup()
   const query = useQuery({
-    queryKey: ['groups'],
+    queryKey: ['groups'] as ValidQueryKeys[],
     queryFn: async () => {
       const groups = await getAllGroupsQuery.execute()
       return groups
